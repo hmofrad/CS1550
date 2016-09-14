f78e76536be1327e87be576479ba257896c988be
Mohammad H. Mofrad <mohammad.h.mofrad@gmail.com>
2016-09-14 12:05:06 -0400

    Create processes.c

---
+++ processes.c
@@ -0,0 +1,38 @@
+/*
+ * CS 1550: Parent & child processes using fork()
+ * Compile: gcc -o processes processes.c
+ * Run: ./processes
+ * (C) Mohammad H. Mofrad, 2016
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void printt(pid_t a)
+{
+    while(1)
+    {
+        printf("Hello from process %d - A\n", a);
+        printf("Hello from process %d - B\n", a);
+        sleep(1);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    pid_t p1 = getpid();
+    pid_t p2 = 0;
+    printf("PID of parent process %d\n", p1);
+
+    if(p1 == getpid())
+    {
+        p2 = fork();
+        if(p2)
+        {
+             printf("PID of child process %d\n", p2);
+        }
+    }
+
+    printt(getpid());
+
+    return 0;
+}
