007aa760bd91c941060ac0c389a79818cf81da8a
Mohammad H. Mofrad <mohammad.h.mofrad@gmail.com>
2016-10-06 23:32:32 -0400

    Create lib.h

---
+++ make/lib.h
@@ -0,0 +1,16 @@
+/* 
+ * Strcpy and strcat header file
+ * (C) Mohammad H. Mofrad, 2016
+ *   
+ */
+ 
+#ifndef _LIB_INCLUDED_H
+#define _LIB_INCLUDED_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void my_strcpy(char *, char *);
+void my_strcat(char *, char *);
+
+#endif
