121891d6a6aa4e4ed7816e0f4be1d37654efd25e
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2016-10-22 12:29:05 -0400

    Update boot.sh

--- project2/boot.sh
+++ project2/boot.sh
@@ -3,9 +3,20 @@
 # (C) Mohammad H. Mofrad, 2016
 
 if [ $# -ne 1 ]; then
-    echo Usage: ./test.sh PITT_ID
+    echo "Usage: ./test.sh PITT_ID"
     exit 1
 fi
+
+if [ -f "/boot/bzImage-devel" ]; then
+     rm -rf /boot/bzImage-devel
+     echo "Cleaning previous bzImage"
+fi
+
+if [ -f "/boot/System.map-devel" ]; then
+     rm -rf /boot/System.map-devel
+     echo "Cleaning previous System.map"
+fi
+
 
 # Change the directory based on your working directory on thoth
 scp $1@thoth.cs.pitt.edu:/u/OSLab/$1/project2/demo/\{bzImage,System.map,prodcons\} ./
