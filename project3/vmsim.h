8d329f4b3dfea64fd2454e17db2e5db21b2169c8
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-11-03 12:42:53 -0400

    vmsim.h: Renaming vimsim to vmsim

---
+++ project3/vmsim.h
@@ -0,0 +1,16 @@
+/*
+ * CS 1550: Header file for Virtual Memory skeleton code
+ * (C) Mohammad H. Mofrad, 2016
+ */
+
+#ifndef _VMSIM_INCLUDED_H
+#define _VMSIM_INCLUDED_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+// TODO: Define page data structure
+
+#endif
