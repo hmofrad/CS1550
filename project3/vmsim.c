6fa7731973e38854b10e70aa9f69b9f56b7c7f80
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-11-03 13:00:45 -0400

    vmsim.c:

--- project3/vmsim.c
+++ project3/vmsim.c
@@ -20,14 +20,33 @@ int main(int argc, char *argv[])
    if((argc == 6) && (!strcmp(argv[1],"-n")) && (!strcmp(argv[3], "-a")) && (!strcmp(argv[4], "fifo")) \
                   && ((!strcmp(argv[5], "gcc.trace")) || (!strcmp(argv[5], "swim.trace"))))
    {
-      file = open(argv[3],"rb");
+      numframes = atoi(argv[2]);
+      file = fopen(argv[5],"rb");
+      if(!file)
+      {
+         fprintf(stderr, "USAGE: %s -n <numframes> -a <fifo> <tracefile>\n", argv[0]);
+         fprintf(stderr, "Error on opening the trace file\n");
+         exit(1); 
+      }
    }
    else
    {
-
       fprintf(stderr, "USAGE: %s -n <numframes> -a <fifo> <tracefile>\n", argv[0]);
       exit(1); 
    }
+
+   printf("%d\n", numframes);
+
+   if(!fclose(file))
+   {
+      return(0);
+   }
+   else
+   { 
+      fprintf(stderr, "Error on closing the trace file\n");
+      exit(1);
+   }
+
 
 
 
