bc3cf9b5b99d6a7dade8cbb7ca77e7285a11f753
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-11-03 15:31:00 -0400

    vmsim.c:

--- project3/vmsim.c
+++ project3/vmsim.c
@@ -6,7 +6,6 @@
  
 #include "vmsim.h"
 
-int numframes;
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +15,7 @@ int main(int argc, char *argv[])
    * and store it in an array
    */
    
+   int numframes;
    FILE *file;
    if((argc == 6) && (!strcmp(argv[1],"-n")) && (!strcmp(argv[3], "-a")) && (!strcmp(argv[4], "fifo")) \
                   && ((!strcmp(argv[5], "gcc.trace")) || (!strcmp(argv[5], "swim.trace"))))
@@ -35,7 +35,45 @@ int main(int argc, char *argv[])
       exit(1); 
    }
 
-   printf("%d\n", numframes);
+   /* 
+    * Calculate the trace file's length
+    * and read in the trace file
+    * and write it into addr_arr and mode_arr arrays 
+    */
+
+   unsigned int len = 0;
+   unsigned int addr = 0;
+   unsigned char mode = NULL;
+
+   // Calculate number of lines in the trace file
+   while(fscanf(file, "%x %c", &addr, &mode) == 2)
+   {
+      len++;
+   }
+   rewind(file);
+
+   unsigned int addr_arr[len];
+   unsigned char mode_arr[len];
+   unsigned int i = 0;
+   // Store the memory accesses 
+   while(fscanf(file, "%x %c", &addr_arr[i], &mode_arr[i]) == 2)
+   {
+      #ifdef DEBUG
+         printf("\'0x%08x %c\'\n", addr_arr[i], mode_arr[i]);
+      #endif
+      i++;
+   }
+
+   //TOBECON
+
+
+
+
+
+
+
+   
+
 
    if(!fclose(file))
    {
