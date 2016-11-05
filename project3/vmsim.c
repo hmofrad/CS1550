c8220d53d7d3c9fc848da152ddcba3e91ce903d3
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-11-04 20:47:06 -0400

    vmsim.c: Bugfix for close

--- project3/vmsim.c
+++ project3/vmsim.c
@@ -31,18 +31,6 @@ int main(int argc, char *argv[])
          fprintf(stderr, "Error on opening the trace file\n");
          exit(1); 
       }
-
-      if(!fclose(file))
-      {
-         ; // Noop
-      }
-      else
-      {
-         fprintf(stderr, "Error on closing the trace file\n");
-         exit(1);
-      }
-
-
    }
    else
    {
@@ -69,18 +57,31 @@ int main(int argc, char *argv[])
    }
    rewind(file);
 
-   unsigned int addr_arr[len];
-   unsigned char mode_arr[len];
+   unsigned int address_array[len];
+   unsigned char mode_array[len];
    unsigned int i = 0;
 
    // Store the memory accesses 
-   while(fscanf(file, "%x %c", &addr_arr[i], &mode_arr[i]) == 2)
+   while(fscanf(file, "%x %c", &address_array[i], &mode_array[i]) == 2)
    {
       #ifdef ALL
-         printf("\'0x%08x %c\'\n", addr_arr[i], mode_arr[i]);
+         printf("\'0x%08x %c\'\n", address_array[i], mode_array[i]);
       #endif
       i++;
    }
+
+   if(!fclose(file))
+   {
+      ; // Noop
+   }
+   else
+   {
+      fprintf(stderr, "Error on closing the trace file\n");
+      exit(1);
+   }
+
+
+
 
    // Initialize the physical memory address space
    physical_frames = malloc(PAGE_SIZE_4KB * numframes);
@@ -91,18 +92,7 @@ int main(int argc, char *argv[])
    }
    memset(physical_frames, 0, PAGE_SIZE_4KB * numframes);
 
-/*
-   #ifdef DEBUG
-      for(i = 0; i < numframes; i++)
-      {
-         printf("%10d: Adding new memory frame at %ld(0x%08x)\n", i, physical_frames + \
-               (i * PAGE_SIZE_4KB)/PAGE_SIZE_BYTES, physical_frames + \
-               (i * PAGE_SIZE_4KB)/PAGE_SIZE_BYTES);
-      }
-   #endif
-*/
-
-   // Initialize the frame data structure
+   // Create the first frame of the frames linked list
    struct frame_struct *frame = malloc(sizeof(struct frame_struct));
    if(!frame)
    {
@@ -113,23 +103,27 @@ int main(int argc, char *argv[])
 
    // Store the head of frames linked list
    struct frame_struct *head = frame;
-   
+
+   // Initialize the frames linked list
    for(i = 0; i < numframes; i++)
    {
       frame->frame_number = i;
       frame->physical_address = physical_frames + (i * PAGE_SIZE_4KB) / PAGE_SIZE_BYTES;
       frame->virtual_address = NULL;
       frame->pte_pointer = NULL;
-      printf("Frame#%d: Adding a new frame at memory address %ld(0x%08x)\n", i, frame->physical_address, frame->physical_address);
-
+      #ifdef DEBUG
+         printf("Frame#%d: Adding a new frame at memory address %ld(0x%08x)\n", i, frame->physical_address, frame->physical_address);
+      #endif
       frame->next = malloc(sizeof(struct frame_struct));
       frame = frame->next;
       memset(frame, 0, sizeof(struct frame_struct));
 
    }
-
-  
-
+   
+   unsigned int fault_address = 0;
+   unsigned char mode_type = NULL;
+   
+   // Main loop for processing memory Accesses
 
    return(0);
 }
