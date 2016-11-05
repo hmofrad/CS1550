1f7187e908d24df7b21e8e06a9e6af1947d1e1a1
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-11-05 19:53:29 -0400

    vmsim.h: Added newest header file

--- project3/vmsim.h
+++ project3/vmsim.h
@@ -1,7 +1,17 @@
 /*
  * CS 1550: Header file for Virtual Memory skeleton code
- * (C) Mohammad H. Mofrad, 2016
- */
+ * with a single level 32-Bit page table
+ * (c) Mohammad H. Mofrad, 2016
+ * (e) hasanzadeh@cs.pitt.edu
+ *
+ * Level 1 Page Table   PAGE FRAME
+ * 31------------- 12 | 11 ------------- 0
+ * |PAGE TABLE ENTRY  | PHYSICAL OFFSET  |
+ * -------------------------------------
+ * <-------20-------> | <-------12------->
+ *
+*/
+
 
 #ifndef _VMSIM_INCLUDED_H
 #define _VMSIM_INCLUDED_H
@@ -11,32 +21,45 @@
 #include <stdint.h>
 #include <string.h>
 
-#define DEBUG
+// Debug levels
 #define ALL
+#define DEBUG
+#define INFO
 
+
+// 32-Bit page table constants
 #define PAGE_SIZE_4KB   4096
-#define PAGE_SIZE_BYTES 4
+#define PT_SIZE_1MB     1048576
 
+#define PAGE_SIZE_BYTES 4
+#define PTE_SIZE_BYTES  4
+
+// Macros to extract pte/frame index
+#define PTE32_INDEX(x)  (((x) >> 12) & 0xfffff)
+#define FRAME_INDEX(x)  ( (x)        &   0x3ff)
+
+// 32-Bit memory frame data structure
 struct frame_struct
 {
-   int frame_number;
+   unsigned int frame_number;
    unsigned int *physical_address;
-   unsigned int *virtual_address;
+   unsigned int virtual_address;
    struct pte_32 *pte_pointer;
    struct frame_struct *next;
-};
-
-struct page_struct
-{
-   int page_number;
 };
 
 // 32-Bit Root level Page Table Entry (PTE) 
 struct pte_32
 {
    unsigned int present;
-   unsigned int *address;
+   unsigned int dirty;
+   unsigned int *physical_address;
 };
 
+// Handle page fault function
+struct frame_struct * handle_page_fault(unsigned int);
+
+// Fifo page replacement algorithm
+int fifo();
 
 #endif
