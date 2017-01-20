446f7ceae9dd2fa16cfdf38a99295f3bf3b165c6
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2017-01-20 15:23:31 -0500

    Create square.c

---
+++ project1/square.c
@@ -0,0 +1,26 @@
+/* 
+ * Graphics Library driver file
+ * (c) Mohammad H. Mofrad, 2017
+ */
+ 
+#include "graphics.h"
+
+int main(int argc, char** argv)
+{
+   int i;
+
+   init_graphics();
+
+   char key;
+   int x = (640-20)/2;
+   int y = (480-20)/2;
+   
+   clear_screen();
+   draw_line(0xFFFF);
+   sleep_s(5);
+   
+   clear_screen();
+   exit_graphics();
+
+   return (0);
+}
