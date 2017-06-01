b0a91445f220f42a84d0bb68508e3ecdd0039c03
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2017-05-31 23:10:28 -0400

    Update library.c

--- project1/library.c
+++ project1/library.c
@@ -1,5 +1,5 @@
 /*
- * CS 1550: Graphics library skeleton code for Qemu VM
+ * CS 1550: Graphics library code skeleton for Qemu VM
  * WARNING: This code is the minimal implementation of the project 1.
  *          It is not intended to serve as a reference implementation.
  *          Following project guidelines, a complete implementation
@@ -11,9 +11,13 @@
 #include "library.h"
 
 int fid;
+/* http://www.ummon.eu/Linux/API/Devices/framebuffer.html */
 struct fb_var_screeninfo var_info;
 struct fb_fix_screeninfo fixed_info;
+
+/* http://man7.org/linux/man-pages/man3/termios.3.html */
 struct termios terminal_settings;
+
 size_t size;
 color_t *address;
 
@@ -26,22 +30,24 @@ void init_graphics()
         perror("Error opening /dev/fb0");
         exit(1);	    
     }
-
+    
+    /* Determine virtual resolution */
     if(ioctl(fid, FBIOGET_VSCREENINFO, &var_info) == -1)
     {
         perror("Error using ioctl");
         exit(1);
     }
     
+    /* Determine bit depth */
     if(ioctl(fid, FBIOGET_FSCREENINFO, &fixed_info) == -1)
     {
         perror("Error using ioctl");
         exit(1);
     }
     
-    size = var_info.yres_virtual * var_info.xres_virtual * sizeof(color_t); // 640 * 480 * 2 = 614400
-    printf("%d %d %d %d\n", size, var_info.yres_virtual, fixed_info.line_length, fixed_info.smem_len);
+    size = var_info.yres_virtual * fixed_info.line_length; // 614400 = 480 * 1280
     
+    /* Total size of the mmap()’ed region */
     address = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fid, 0);
     if(address == (void *) -1)
     {
@@ -49,13 +55,15 @@ void init_graphics()
         exit(1);
     }
     
+    //  disable keypress echo and buffering the keypresses
     if(ioctl(STDIN_FILENO, TCGETS, &terminal_settings) == -1)
     {
         perror("Error using ioctl");
         exit(1);
     }
-
+    
     terminal_settings.c_lflag &= ~(ICANON | ECHO);
+    
     if(ioctl(STDIN_FILENO, TCSETS, &terminal_settings) == -1)
     {
         perror("Error using ioctl");
@@ -69,42 +77,39 @@ void draw_pixel(int x, int y, color_t color)
     {
         return;
     }
-    int coordinate = (y * var_info.xres_virtual) + x;
+    int coordinate = (y * fixed_info.line_length / sizeof(color_t)) + x;
     *(address + coordinate) = color;
 }
 
 void sleep_s(unsigned seconds)
 {
-   sleep(seconds); /* This is in seconds and not milliseconds */
+   sleep(seconds); // This is in seconds
 }
 
 void clear_screen() 
 {
-    const char cmd[] = "\033[2J";
-    write(STDOUT_FILENO, cmd, sizeof(cmd));  /* This will do the trick for fid1*/
+    const char cmd[] = "\033[2J"; // ANSI escape code
+    write(STDOUT_FILENO, cmd, sizeof(cmd));  // This will do the trick
 }
 
 void exit_graphics() 
 {
-    /* 
-     * Skipping ioctl for reseting the terminal setting for fid1
-     * Remove the memory mapping
-     * Finally, close fb file desriptor
-     */
-    
+    // Reseting the terminal settings
     terminal_settings.c_lflag |= (ICANON | ECHO);
 
     if(ioctl(STDIN_FILENO, TCSETS, &terminal_settings) == -1) {
         perror("Error using ioctl");
         exit(1);
     }
-
+    
+    // Remove the memory mapping
     if(munmap(address, size) == -1)
     {
         perror("Error unmapping memory");
         exit(1);
     }
-
+    
+    // close fb file desriptor
     if(!close(fid))
     {
         exit(0);
