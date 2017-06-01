cc7ce5652806d8e37401d3e58e004091d00855f5
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2017-05-31 22:40:38 -0400

    Update library.h

--- project1/library.h
+++ project1/library.h
@@ -20,10 +20,6 @@
 typedef unsigned short color_t;    // |15 11|10  5|4  0|
                                    // |red  |green|blue|
                                    //   5   + 6   + 5  =  16 bits
-// Macros to extract RGB colors 
-#define BMASK(c) (c & 0x001F) // Blue mask
-#define GMASK(c) (c & 0x07E0) // Green mask
-#define RMASK(c) (c & 0xF800) // Red mask
 
 void clear_screen();
 void init_graphics();
