ae79abb7b8250a0b54e82c545f82d392458e01e5
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2017-05-31 22:45:45 -0400

    Update driver.c

--- project1/driver.c
+++ project1/driver.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
    init_graphics();
    clear_screen();
    for(i = 0; i < 640; i++)
-       draw_pixel(i, 100, 65535); // White color
+       draw_pixel(i, 100, 65535); // White color (0xFFFF)
    sleep_s(5);
    
    clear_screen();
