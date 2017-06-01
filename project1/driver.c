a81dd0da61abb4699816e3556c249e4ee755221c
Mohammad H. Mofrad <m.hasanzadeh.mofrad@gmail.com>
2017-05-31 22:42:57 -0400

    Update and rename square.c to driver.c

--- project1/driver.c
+++ project1/driver.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
    init_graphics();
    clear_screen();
    for(i = 0; i < 640; i++)
-       draw_pixel(i, 100, 255);
+       draw_pixel(i, 100, 65535); // White color
    sleep_s(5);
    
    clear_screen();
