3f2ab932b3b7916da7e873f1722a645599175a25
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-20 01:19:00 -0400

    Update prodcons.h

---
+++ project1/prodcons.h
@@ -0,0 +1,25 @@
+/* 
+ * Skeleton code for prodcons implementation
+ * (C) Mohammad H. Mofrad, 2016  
+ *
+ */
+#ifndef PRODCONS_H_INCLUDED
+#define PRODCONS_H_INCLUDED
+
+
+#include <asm/errno.h>
+#include <asm/unistd.h>
+#include <asm/mman.h>
+
+#define MAP_SIZE 0x00000FFF
+
+struct cs1550_sem
+{
+   int value;
+   //Some process queue of your devising
+};
+
+void  cs1550_down(struct cs1550_sem *);
+void  cs1550_up  (struct cs1550_sem *);
+
+#endif
