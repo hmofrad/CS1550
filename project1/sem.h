7c39ae9d08f4ce2a1fa9d0aa9a677037df385de2
Mohammad Hasanzadeh Mofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-19 22:49:38 -0400

    Add files via upload

---
+++ project1/sem.h
@@ -0,0 +1,16 @@
+/*
+ sem.h: CS1550 semaphore helper library 
+ (c) Mohammad Hasanzadeh Mofrad, 2019
+ (e) moh18@pitt.edu
+*/
+
+#ifndef SEM_H_INCLUDED
+#define SEM_H_INCLUDED
+
+// Your implementation goes here ...
+struct cs1550_sem {
+   int value;
+   //Some process queue of your devising
+};
+
+#endif
