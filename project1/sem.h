c9fcb20d985cce13e52d8216b8e692cc116b6425
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-20 02:48:34 -0400

    Update sem.h

--- project1/sem.h
+++ project1/sem.h
@@ -6,10 +6,6 @@
 
 #ifndef SEM_H_INCLUDED
 #define SEM_H_INCLUDED
-
-#include <asm/errno.h>
-#include <asm/unistd.h>
-// Your implementation goes here ...
 struct cs1550_sem {
    int value;
    //Some process queue of your devising
@@ -17,16 +13,4 @@ struct cs1550_sem {
 
 void  down(struct cs1550_sem *);
 void  up  (struct cs1550_sem *);
-
-/*
-void down(struct cs1550_sem *sem) {
-  syscall(__NR_cs1550_down, sem);
-}
-
-void up(struct cs1550_sem *sem) {
-  syscall(__NR_cs1550_up, sem);
-}
-*/
-
-
 #endif
