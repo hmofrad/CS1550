a38f19961f6b92662003214896ea901d44be59af
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-20 01:14:47 -0400

    Update unistd.h

--- project1/unistd.h
+++ project1/unistd.h
@@ -330,15 +330,14 @@
 #define __NR_timerfd		322
 #define __NR_eventfd		323
 #define __NR_fallocate		324
-
-/* BEGIN */
+/* Added by Mohammad H. Mofrad */
 #define __NR_sys_cs1550_down	325
-#define __NR_sys_cs1550_up	    326
+#define __NR_sys_cs1550_up	326
 
 #ifdef __KERNEL__
 
+/* Changed by Mohammad H. Mofrad */
 #define NR_syscalls 327
-/* END */
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
