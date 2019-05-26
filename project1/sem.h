11ca0d81d9e9687c44776a2a070b771b7542e342
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-25 23:07:16 -0400

    Update sem.h

--- project1/sem.h
+++ project1/sem.h
@@ -7,7 +7,12 @@
 #ifndef SEM_H_INCLUDED
 #define SEM_H_INCLUDED
 struct cs1550_sem {
-   int value;
-   //Some process queue of your devising
+    int value;
+    struct cs1550_queue *head;
+};
+
+struct cs1550_queue {
+    struct cs1550_queue *next;
+    struct task_struct *task;
 };
 #endif
