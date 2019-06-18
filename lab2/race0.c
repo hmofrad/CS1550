99b8c8525cde2a1a55b544c6f6cf5d05a71c2549
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-06-17 22:53:18 -0400

    Update race0.c

---
+++ lab2/race0.c
@@ -0,0 +1,38 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "condvar.h"
+
+//We want Child 1 to execute first, then Child 2, and finally Parent.
+int main() {
+    struct condvar cv;
+    init_lock(&cv.lk);
+    int pid = fork(); //fork the first child
+    if(pid < 0) {
+        printf(1, "Error forking first child.\n");
+    } else if (pid == 0) {
+        printf(1, "Child 1 Executing\n");
+        lock(&cv.lk);
+        cv_signal(&cv);
+        unlock(&cv.lk);
+    } else {
+        pid = fork(); //fork the second child
+        if(pid < 0) {
+            printf(1, "Error forking second child.\n");
+        } else if(pid == 0) {
+            printf(1, "Child 2 Executing\n");
+            lock(&cv.lk);
+            cv_wait(&cv);
+            unlock(&cv.lk);
+        } else {
+            printf(1, "Parent Waiting\n");
+            int i;
+            for(i=0; i< 2; i++)
+                wait();
+            printf(1, "Children completed\n");
+            printf(1, "Parent Executing\n");
+            printf(1, "Parent exiting.\n");
+        }
+    }
+    exit();
+}
